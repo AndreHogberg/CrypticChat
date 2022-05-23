f40487c12f8a2e88c27502bd1edeb9cb27de7808
André Högberg <andrehogberg1337@gmail.com>
2022-05-23 09:24:09 +0200

    Feat: Added some more projects to the backend.

---
+++ CrypticChat-Backend/CrypticChat.Domain/AppUser.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CrypticChat.Domain;
+
+public class AppUser : IdentityUser
+{
+}
---
+++ CrypticChat-Backend/CrypticChat.Persistance/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using CrypticChat.Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrypticChat.Persistance;
+
+public class DataContext : IdentityDbContext<AppUser>
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+        
+    }
+}
