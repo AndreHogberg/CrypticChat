f4ba5cfc63d4729ab67b422c94005fb8b469a9bf
OscarKarlsson <70796269+OscarKarlsson@users.noreply.github.com>
2022-05-27 11:38:50 +0200

    Feat: Added Redux and StateSlices

---
+++ CrypticChat-Client/src/redux/slices/userSlice.ts
@@ -0,0 +1,39 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { Action } from "history";
+import type { RootState } from "../store";
+
+// Define a type for the slice state
+interface userState {
+  userName: string;
+  token: string;
+}
+
+// Define the initial state using that type
+const initialState: userState = {
+  userName: "",
+  token: "",
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  // `createSlice` will infer the state type from the `initialState` argument
+  initialState,
+  reducers: {
+    // Use the PayloadAction type to declare the contents of `action.payload`
+    setUserName: (state, action: PayloadAction<string>) => {
+      state.userName = action.payload;
+    },
+    setToken: (state, action: PayloadAction<string>) => {
+      state.token = action.payload;
+    },
+  },
+});
+
+export const { setUserName, setToken } = userSlice.actions;
+
+// Other code such as selectors can use the imported `RootState` type
+export const getUserName = (state: RootState) => state.user.userName;
+
+export const getToken = (state: RootState) => state.user.token;
+
+export default userSlice.reducer;
