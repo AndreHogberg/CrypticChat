2ec315e776a05ed6ff79475799c04f64d79615bf
OscarKarlsson <70796269+OscarKarlsson@users.noreply.github.com>
2022-06-02 10:52:47 +0200

    Feat: Login/Register frontend

--- CrypticChat-Client/src/main.tsx
+++ CrypticChat-Client/src/main.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Provider } from "react-redux";
 import App from "./App";
 import "./index.css";
+import { store } from "./redux/store";
+import login from "./components/login";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider>
-        <App />
+      <Provider store={store}>
+        <Routes>
+          <Route path="/login" element={<login />} />
+        </Routes>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
