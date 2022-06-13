45c11bb957d7db2e9e300db4396fffc249bc9b5b
hogberg <andrehogberg1337@gmail.com>
2022-06-13 12:11:39 +0200

    Fix: Fixed chatConnection to the correct room

--- CrypticChat-Client/src/components/Chat/ChatBox.tsx
+++ CrypticChat-Client/src/components/Chat/ChatBox.tsx
@@ -8,54 +8,72 @@ import { useAppSelector } from "../../redux/hooks";
 import Incoming from "./Incoming";
 import Outgoing from "./Outgoing";
 
-
 interface Props {
-    friendId: string
-    chatRoomId: string
+  friendId: string;
+  chatRoomId: string;
 }
 
-export default function ChatBox({friendId, chatRoomId}: Props){
-    const [inputText, setInputText] = useState("");
-    const {user,chat} = useAppSelector((state) => state);
-    const [messages, setMessage] = useState<UserMessage[]>([]);
-    const [chatId, setChatId] = useState("");
-    chatConnection.invoke("ConnectToRoom", chatRoomId);
+export default function ChatBox({ friendId, chatRoomId }: Props) {
+  const [inputText, setInputText] = useState("");
+  const { user, chat } = useAppSelector((state) => state);
+  const [messages, setMessage] = useState<UserMessage[]>([]);
+  const [chatId, setChatId] = useState("");
 
-    useMemo(() => {
-      setMessage(state => {
-        if(chat.message){
-          state.push(chat.message);
-        }
-        return state;
-      })
-    },[chat])
+  useMemo(() => {
+    setMessage((state) => {
+      if (chat.message) {
+        state.push(chat.message);
+      }
+      return state;
+    });
+  }, [chat]);
 
-    useEffect(() => {
-      setMessage([]);
-      agent.Messages.newChat(friendId).then(mes => {
-        setMessage(mes.messages);
-        setChatId(mes.chatRoomId);
-      });
-    },[friendId])
+  useEffect(() => {
+    setMessage([]);
+    agent.Messages.newChat(friendId).then((mes) => {
+      setMessage(mes.messages);
+      setChatId(mes.chatRoomId);
+    });
+  }, [friendId]);
 
-    const click = () => {
-      chatConnection.invoke("NewMessage", inputText, chatRoomId);
-      setInputText("");
-    }
-    return (
-        <div className="flex flex-col w-full h-full px-2 py-2 bg-gradient-to-b justify-between space-y-5 from-white to-gray-90">
-        <div className="flex flex-col w-full h-full overflow-auto">
-          {messages && messages.map(um => (
-              um.sender === user.userName ? <Outgoing Text={um.message} Name={um.sender} Date={new Date(um.date)}/> 
-              : <Incoming Text={um.message} Name={um.sender} Date={new Date(um.date)}/>
-          ))}
-        </div>
-        <div className="flex flex-roww-full justify-end items-center">
-          <input value={inputText}  placeholder="Send Oscar a message" className="p-1 border-black w-full" onChange={(e) => setInputText(e.target.value)}></input>
-          <button className="bg-purple-600 hover:bg-purple-700 px-2 flex-none py-2 m-2 border-black rounded text-white" onClick={click}>
-            <BsFillArrowRightCircleFill />
-          </button>
-        </div>
+  const click = () => {
+    chatConnection.invoke("NewMessage", inputText, chatRoomId);
+    setInputText("");
+  };
+  return (
+    <div className="flex flex-col w-full h-full px-2 py-2 bg-gradient-to-b justify-between space-y-5 from-white to-gray-90">
+      <div className="flex flex-col w-full h-full overflow-auto">
+        {messages &&
+          messages.map((um) =>
+            um.sender === user.userName ? (
+              <Outgoing
+                Text={um.message}
+                Name={um.sender}
+                Date={new Date(um.date)}
+              />
+            ) : (
+              <Incoming
+                Text={um.message}
+                Name={um.sender}
+                Date={new Date(um.date)}
+              />
+            )
+          )}
       </div>
-    )
+      <div className="flex flex-roww-full justify-end items-center">
+        <input
+          value={inputText}
+          placeholder="Send Oscar a message"
+          className="p-1 border-black w-full"
+          onChange={(e) => setInputText(e.target.value)}
+        ></input>
+        <button
+          className="bg-purple-600 hover:bg-purple-700 px-2 flex-none py-2 m-2 border-black rounded text-white"
+          onClick={click}
+        >
+          <BsFillArrowRightCircleFill />
+        </button>
+      </div>
+    </div>
+  );
 }
--- CrypticChat-Client/src/components/SideBar/Chat.tsx
+++ CrypticChat-Client/src/components/SideBar/Chat.tsx
@@ -1,10 +1,12 @@
 import { useParams, useSearchParams } from "react-router-dom";
+import chatConnection from "../../lib/signalR";
 import ChatBox from "../Chat/ChatBox";
 
 export default function Chat() {
   const param = useParams<{ id: string }>();
   const [searchParams, setSearchParams] = useSearchParams();
   const chatRoom = searchParams.get("chatRoom");
+  chatConnection.invoke("ConnectToRoom", chatRoom);
 
   return <ChatBox friendId={param.id!} chatRoomId={chatRoom!} />;
 }
