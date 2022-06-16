8142e6f5f8f2f449c2fbb3a6493a450550f2ecd2
OscarKarlsson <70796269+OscarKarlsson@users.noreply.github.com>
2022-06-16 12:39:50 +0200

    WIP: design

--- CrypticChat-Client/src/components/Welcome.tsx
+++ CrypticChat-Client/src/components/Welcome.tsx
@@ -1,7 +1,15 @@
-export default function Welcome(){
-    return (
-        <div>
-            Starter page 
-        </div>
-    )
+import { useAppSelector } from "../redux/hooks";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
+export default function Welcome() {
+  const user = useAppSelector((state) => state.user);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!user.Authenticated) {
+      console.log("Hello");
+      navigate("/login");
+    }
+  }, [user]);
+  return <div>Starter page</div>;
 }
